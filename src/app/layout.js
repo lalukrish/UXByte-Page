848e6b48a814f93f3ed33995159e526f8c60b1d2
Adithya <jaiadithya1400@gmail.com>
2024-03-28 23:18:25 +0530

    footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "UXByte",
