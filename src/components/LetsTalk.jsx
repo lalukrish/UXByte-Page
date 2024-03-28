241abfb1df368d3f582b919129adbca4121fde7d
Adithya <jaiadithya1400@gmail.com>
2024-03-29 00:45:26 +0530

    lets

--- src/components/LetsTalk.jsx
+++ src/components/LetsTalk.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const LetsTalk = () => {
   return (
-    <div>LetsTalk</div>
+    <div className='flex justify-center' id='lets_talk'>
+
+    <div className='text-6xl font-extrabold	mb-8'>
+  <span className="bg-gradient-to-r from-purple-500 to-purple-800 bg-clip-text text-transparent">
+    Let's talk
+  </span>
+</div>
+
+<div>
+
+</div>
+</div>
   )
 }
 
