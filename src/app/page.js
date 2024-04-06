8e29cc77f8f17e68e3261c99590111831268cef9
lalkrishna <lalukrishnavallil01@gmail.com>
2024-04-06 18:58:14 +0530

    animation started

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div suppressHydrationWarning={false}>
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
       <About />
       <WhyUs />
       <Works />
